49646a2e8927554f89e01c7207813352d322637e
Andre Luiz <andreluiz.jpofi@gmail.com>
2024-08-16 17:19:40 -0300

    insert user with POTS

--- src/main/java/io/drezzy/workshopmongo/services/UserService.java
+++ src/main/java/io/drezzy/workshopmongo/services/UserService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import dto.UserDTO;
 import io.drezzy.workshopmongo.domain.User;
 import io.drezzy.workshopmongo.repository.UserRepository;
 import io.drezzy.workshopmongo.services.exception.ObjectNotFoundException;
@@ -23,5 +24,11 @@ public class UserService {
     }
     public User findById(String id){
         return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("User not found"));
+    }
+    public User insert(User user){
+        return repo.insert(user);
+    }
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
